1637f4c7d6ec88d0ed1d26c3a88db603b0d7ce2c
Diego Barcellos <diegocb81@gmail.com>
2025-07-09 00:38:07 -0300

    Montando a Forca e os Espaços das Letras - Parte 2

--- src/main/java/br/com/dio/hangman/model/HangmanGame.java
+++ src/main/java/br/com/dio/hangman/model/HangmanGame.java
@@ -6,14 +6,22 @@ import static br.com.dio.hangman.model.HangmanGameStatus.PENDING;
 
 public class HangmanGame {
 
+    private final static int HANGMAN_INITIAL_LINE_LENGTH = 9;
+    private final static int HANGMAN_INITIAL_LINE_LENGTH_WITH_LINE_SEPARATOR = 10;
+
+    private final int lineSize;
+    private final List<HangmanChar> characters;
+
     private String hangman;
     private HangmanGameStatus hangmanGameStatus;
 
     public HangmanGame(final List<HangmanChar> caracters) {
         var whiteSpaces = " ".repeat(caracters.size());
         var characterSpace = "-".repeat(caracters.size());
+        this.lineSize = HANGMAN_INITIAL_LINE_LENGTH_WITH_LINE_SEPARATOR + whiteSpaces.length();
         this.hangmanGameStatus = PENDING;
         buildHangmanDesign(whiteSpaces, characterSpace);
+        this.characters = setCharacterSpacesPositionInGame(caracters, whiteSpaces.length());
     }
 
     @Override
@@ -21,14 +29,22 @@ public class HangmanGame {
         return this.hangman;
     }
 
+    private List<HangmanChar> setCharacterSpacesPositionInGame(final List<HangmanChar> characters, final int whiteSpacesAmount){
+        final var LINE_LETTER = 6;
+        for (int i = 0; i < characters.size(); i++) {
+            characters.get(i).setPosition(this.lineSize * LINE_LETTER + HANGMAN_INITIAL_LINE_LENGTH + i);
+        }
+        return characters;
+    }
+
     private void buildHangmanDesign(final String whiteSpaces, final String characterSpace) {
         this.hangman = "  -----  " + whiteSpaces + System.lineSeparator() +
-                  "  |   |  " + whiteSpaces + System.lineSeparator() +
-                  "  |   |  " + whiteSpaces + System.lineSeparator() +
-                  "  |      " + whiteSpaces + System.lineSeparator() +
-                  "  |      " + whiteSpaces + System.lineSeparator() +
-                  "  |      " + whiteSpaces + System.lineSeparator() +
-                  "  |      " + whiteSpaces + System.lineSeparator() +
-                  "=========" + whiteSpaces + System.lineSeparator();
+                       "  |   |  " + whiteSpaces + System.lineSeparator() +
+                       "  |   |  " + whiteSpaces + System.lineSeparator() +
+                       "  |      " + whiteSpaces + System.lineSeparator() +
+                       "  |      " + whiteSpaces + System.lineSeparator() +
+                       "  |      " + whiteSpaces + System.lineSeparator() +
+                       "  |      " + whiteSpaces + System.lineSeparator() +
+                       "=========" + characterSpace + System.lineSeparator();
     }
 }
